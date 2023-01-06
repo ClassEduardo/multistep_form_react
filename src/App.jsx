5220b5155be8e0f111e709dfb6fe27e4a3ac93ce
Eduardo Gonç <dudue0578@gmail.com>
2023-01-05 16:47:04 -1200

    Estrutura básica da aplicação

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
+import {GrFormNext, GrFormPrevious} from 'react-icons/gr';
 import './App.css'
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div className='header'>
+        <h2>Deixe sua avaliação</h2>
+        <p>
+          Ficamos felizes com sua compra, utilize o formulário a baixo para avaliar o podruto
+        </p>
+      </div>
+      <div className='form-container'>
+        <p>Etapas</p>
+        <form>
+
+          <div className="inputs-container"></div>
+
+          <div className='actions'>
+            <button type='button'>
+              <GrFormPrevious />
+              <span>Voltar</span>
+            </button>
+            <button type='submit'>
+              <span>Avançar</span>
+              <GrFormNext />
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
-
-export default App
